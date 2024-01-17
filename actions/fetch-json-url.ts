5ee842a4ccb71da1c2a96fb39205f33b5ce267fb
Scott Weaver <scottmweaver@gmail.com>
2024-01-17 11:30:27 -0800

    #11: Add URL Input to JSON Formatter

---
+++ actions/fetch-json-url.ts
@@ -0,0 +1,23 @@
+"use server";
+
+export default async function fetchJsonUrl(url: string) {
+  try {
+    const resp = await fetch(url);
+
+    // Get http status.
+    const status = resp.status;
+
+    if (status !== 200) {
+      throw new TypeError("Error getting data");
+    }
+
+    const text = await resp.json();
+
+    return text;
+  } catch (err: any) {
+    if (err instanceof TypeError) {
+      return { error: err.message };
+    }
+    console.error(err?.message);
+  }
+}
--- components/pages/json-formatter.tsx
+++ components/pages/json-formatter.tsx
@@ -2,11 +2,42 @@
 
 import { useState } from "react";
 
-import TitleHeader from "@/components/title-header";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import CodeText from "@/components/code-text";
+import fetchJsonUrl from "@/actions/fetch-json-url";
+import TitleHeader from "@/components/title-header";
+import isValidUrl from "@/lib/is-valid-url";
+
+import { Loader } from "lucide-react";
 
 export default function JSONFormatterPage() {
-  const [json, setJSON] = useState("");
+  const [json, setJSON] = useState<string>("");
+  const [fetchUrl, setFetchUrl] = useState<string>("");
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  const handleClickFetch = async () => {
+    if (!!fetchUrl && isValidUrl(fetchUrl)) {
+      setError("");
+      setIsFetching(true);
+      try {
+        const data = await fetchJsonUrl(fetchUrl);
+
+        if (data.error) {
+          setError(data.error);
+          setIsFetching(false);
+          return;
+        }
+
+        setJSON(data);
+        setIsFetching(false);
+      } catch (err) {
+        console.error(err);
+        setIsFetching(false);
+      }
+    }
+  };
 
   return (
     <section className="flex flex-col gap-2 w-full">
@@ -15,9 +46,28 @@ export default function JSONFormatterPage() {
         subtitle="Paste in valid JSON and it will be formatted for you."
         showCopy={false}
       />
-      <div className="sticky top-0 bg-white">
+      <div className="sticky flex flex-col gap-4 top-0 bg-white">
+        <div className="flex flex-row">
+          <Input
+            className="rounded-tr-none rounded-br-none"
+            defaultValue={fetchUrl as string}
+            disabled={isFetching}
+            onChange={(e) => setFetchUrl(e.target.value)}
+            placeholder="https://..."
+            title="URL"
+          />
+          <Button
+            className="rounded-tl-none rounded-bl-none"
+            disabled={isFetching || !isValidUrl(fetchUrl)}
+            onClick={handleClickFetch}
+            variant={isFetching ? "ghost" : "default"}
+          >
+            {isFetching ? <Loader className="animate-spin" size={24} /> : "Fetch"}
+          </Button>
+        </div>
         <CodeText
           initialValue={json && json.length > 0 ? JSON.stringify(json) : ""}
+          externalError={error}
           onFormatSuccess={(json) => setJSON(json)}
           placeholder="Type or paste code here..."
         />
